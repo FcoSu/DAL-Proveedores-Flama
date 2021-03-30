efdaf5cd7c57db18824c9c7e76e9135e1bc133b2
Francisco <panchoseur@gmail.com>
2021-03-30 11:40:46 -0300

    first commit

---
+++ src/main/java/com/everis/latam/DALProveedoresFlama/DalProveedoresFlamaApplication.java
@@ -0,0 +1,13 @@
+package com.everis.latam.DALProveedoresFlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalProveedoresFlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalProveedoresFlamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/latam/DALProveedoresFlama/DalProveedoresFlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.latam.DALProveedoresFlama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalProveedoresFlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
